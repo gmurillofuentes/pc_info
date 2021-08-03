0614729334e16892f19249f4247d2df123692ccf
gmurillofuentes <murillex1@hotmail.com>
2021-08-03 17:49:50 -0600

    Create configuracion.py

---
+++ configuracion.py
@@ -0,0 +1,7 @@
+configuracion = {
+    "ws": "http://localhost/webService/utilidades/guardaInventarioEquipo",
+    "version_number": "1.1",
+    "version_file": "https://raw.githubusercontent.com/gmurillofuentes/pc_info/main/version.txt",
+    "file_location": "https://github.com/gmurillofuentes/pc_info/raw/main",
+    "local_file": "C:\python_proyects",
+}
