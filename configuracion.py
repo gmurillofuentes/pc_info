91a2f04b3e01b443b39b54685122b87e82c025d5
gmurillofuentes <murillex1@hotmail.com>
2021-08-03 18:04:57 -0600

    Update configuracion.py

--- configuracion.py
+++ configuracion.py
@@ -2,6 +2,8 @@ configuracion = {
     "ws": "http://localhost/webService/utilidades/guardaInventarioEquipo",
     "version_number": "1.1",
     "version_file": "https://raw.githubusercontent.com/gmurillofuentes/pc_info/main/version.txt",
-    "file_location": "https://github.com/gmurillofuentes/pc_info/raw/main",
-    "local_file": "C:\python_proyects",
+    "file_location": "https://github.com/gmurillofuentes/pc_info/raw/main/pc_info.exe",
+    "local_file": "C:\python_proyects\pc_info.exe",
+    "file_configuracion": "https://github.com/gmurillofuentes/pc_info/raw/main/configuracion.py",
+    "local_configuracion": "C:\python_proyects\configuracion.py",    
 }
